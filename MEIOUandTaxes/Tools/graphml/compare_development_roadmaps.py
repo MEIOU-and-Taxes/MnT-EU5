3af9bf60ce24e21c34f95d5b94355a4f04d91d24
Adelin <ademost95@yahoo.com>
2025-10-11 02:48:55 +0800

    feat: Compare parents & show movement. Make comparisons more stable. Split code
    Previous revision's comparison was unstable because upon .graphml edit, reindexing happens. The fix is to assign ID custom parameter to all nodes.

--- MEIOUandTaxes/Tools/graphml/compare_development_roadmaps.py
+++ MEIOUandTaxes/Tools/graphml/compare_development_roadmaps.py
@@ -8,51 +8,34 @@
 # And run the PyCharm debugger on the specified port.
 
 import sys
-from typing import Any
 
-from parse import parse_graphml
+from diff import get_graph_diff_ids
 from auxiliary import enable_debugging
-
-
-def get_graph_diff_ids(edges_new: dict[Any, Any], edges_old: dict[Any, Any], nodes_new: dict[Any, Any],
-					   nodes_old: dict[Any, Any]) -> tuple[
-	list[Any], list[Any], list[Any], list[Any], list[Any], list[Any]]:
-	# Get the sets of IDs
-	old_node_ids = set(nodes_old.keys())
-	new_node_ids = set(nodes_new.keys())
-	old_edge_ids = set(edges_old.keys())
-	new_edge_ids = set(edges_new.keys())
-
-	# Find added, removed, and common nodes
-	added_node_ids = sorted(new_node_ids - old_node_ids)
-	removed_node_ids = sorted(old_node_ids - new_node_ids)
-	common_node_ids = sorted(old_node_ids & new_node_ids)
-
-	added_edge_ids = sorted(new_edge_ids - old_edge_ids)
-	removed_edge_ids = sorted(old_edge_ids - new_edge_ids)
-	common_edge_ids = sorted(old_edge_ids & new_edge_ids)
-
-	return added_edge_ids, added_node_ids, common_edge_ids, common_node_ids, removed_edge_ids, removed_node_ids
+from parse import process_graphml_file
 
 
 def main():
-
 	enable_debugging()
 	# Git provides several arguments; the old and new file paths are reliable ones
 	file_path_first = sys.argv[2]
 	file_path_second = sys.argv[1]
 
 	# Parse both files to get their node & edge structures
-	nodes_first, edges_first = parse_graphml(file_path_first)
-	nodes_second, edges_second = parse_graphml(file_path_second)
+	nodes_first, edges_first, can_compare_first_file = process_graphml_file(file_path_first)
+	nodes_second, edges_second, can_compare_second_file = process_graphml_file(file_path_second)
 
-	added_edge_ids, added_node_ids, common_edge_ids, common_node_ids, removed_edge_ids, removed_node_ids = get_graph_diff_ids(
+	if not can_compare_first_file:
+		print("Can't compare because the first file has no ID custom parameter - cannot use this revision")
+		return
+	if not can_compare_second_file:
+		print("Can't compare because the second file has no ID custom parameter - cannot use this revision")
+		return
+
+	diff_ids_set  = get_graph_diff_ids(
 		edges_second, edges_first, nodes_second, nodes_first)
 
-	from render import print_elements
-	print_elements(added_edge_ids, added_node_ids, common_edge_ids, common_node_ids, edges_second, edges_first,
-				   nodes_second,
-				   nodes_first, removed_edge_ids, removed_node_ids)
+	from render import print_diff_report
+	print_diff_report(nodes_first, nodes_second, edges_first, edges_second, diff_ids_set)
 
 
 if __name__ == "__main__":
