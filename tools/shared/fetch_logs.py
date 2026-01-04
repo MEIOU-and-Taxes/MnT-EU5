2b3db0d16d8f4a3f2c83fa00cb127ce0e51f05c5
Adelin <ademost95@yahoo.com>
2026-01-05 05:15:25 +0800

    Export PM profitability & age to Excel; Generalize config getter (#138)

--- tools/shared/fetch_logs.py
+++ tools/shared/fetch_logs.py
@@ -1,9 +1,10 @@
 import configparser
 import os
+import sys
 
 CONFIG_PATH = r"../shared/"
 CONFIG_FILE = CONFIG_PATH + r"config.ini"
-DEFAULT_LOG_PATH = ''
+DEFAULT_PATH = ''
 
 def _create_default_config():
 	"""
@@ -13,7 +14,10 @@ def _create_default_config():
 	try:
 		os.makedirs(CONFIG_PATH, exist_ok=True)
 		config = configparser.ConfigParser()
-		config['Paths'] = {'log_directory': DEFAULT_LOG_PATH}
+		config['Paths'] = {
+			'log_directory': DEFAULT_PATH,
+			'game_directory': DEFAULT_PATH
+		}
 		with open(CONFIG_FILE, 'w') as configfile:
 			config.write(configfile)
 		print("INFO: Please edit the 'log_directory' path in the new config file and restart the application.")
@@ -21,7 +25,7 @@ def _create_default_config():
 		print(f"ERROR: Could not create or write to the config file: {e}")
 
 
-def get_log_directory_from_config():
+def get_from_config(section, option):
 	"""
 	Reads the log directory path from the config file.
 	If the config file does not exist, it will be created with a default value.
@@ -29,16 +33,17 @@ def get_log_directory_from_config():
 	if not os.path.exists(CONFIG_FILE):
 		_create_default_config()
 		# After creation, we know the value is the default, so we can return it directly.
-		return DEFAULT_LOG_PATH
+		print("Config file created. Provide it with values for the properties necessary to run the script")
+		sys.exit(1)
 
 	config = configparser.ConfigParser()
 	try:
 		config.read(CONFIG_FILE)
-		return config.get('Paths', 'log_directory')
+		return config.get(section, option)
 	except (configparser.NoSectionError, configparser.NoOptionError):
 		# This handles cases where the file exists but is empty or malformed.
-		print(f"WARNING: Config file at '{CONFIG_FILE}' is missing the required 'Paths' section or 'log_directory' key. Using default path.")
-		return DEFAULT_LOG_PATH
+		print(f"ERROR: Config file at '{CONFIG_FILE}' is missing the required {section} section or {option} key")
+		sys.exit(1)
 	except Exception as e:
 		print(f"ERROR: An unexpected error occurred while reading the config file: {e}")
-	return DEFAULT_LOG_PATH
+	return DEFAULT_PATH
